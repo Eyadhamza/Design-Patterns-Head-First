a4f216d6aae57416b55394d8c5a8c1b856933df6
EyadHamza <EyadHamza0@outlook.com>
2021-08-10 19:19:40 +0200

    Strategy

---
+++ Head First Design Patterns/Duck.cs
@@ -0,0 +1,8 @@
+﻿namespace Head_First_Design_Patterns
+{
+    public abstract class Duck
+    {
+        protected IQuackBehavior _quackBehavior;
+        protected IFlyBehavior _flyBehavior;
+    }
+}
---
+++ Head First Design Patterns/MallardDuck.cs
@@ -0,0 +1,11 @@
+﻿namespace Head_First_Design_Patterns
+{
+    public class MallardDuck : Duck
+    {
+        public MallardDuck()
+        {
+             _quackBehavior = new Quack();
+             _flyBehavior = new FlyWithWings();
+        }
+    }
+}
